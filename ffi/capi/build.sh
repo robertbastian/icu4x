a6becfce7c2a9b2bf9e09173995e2d60fc9c2749
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-01-08 12:06:46 +0100

    wasm

--- ffi/capi/build.sh
+++ ffi/capi/build.sh
@@ -1,13 +1,15 @@
 NIGHTLY="${PINNED_CI_NIGHTLY:=nightly-2025-09-27}"
 
 case $TARGET in
-    "riscv64-linux-android" | "riscv64gc-unknown-linux-gnu" | "wasm32-unknown-unknown") 
+    "riscv64-linux-android" | "riscv64gc-unknown-linux-gnu") 
         NO_STD="1" ;;
+    "wasm32-unknown-unknown")
+        NO_STD="2" ;;
     *) 
         NO_STD="0" ;;
 esac
 
-if [[ "$TYPE" = "static" ]] || [[ $NO_STD == 1 ]]; then
+if [[ "$TYPE" = "static" ]] || [[ $NO_STD > 0 ]]; then
     rustup toolchain install --no-self-update $NIGHTLY --component rust-src
     rustup target add $TARGET --toolchain $NIGHTLY
 else
@@ -20,12 +22,12 @@ fi
 # --config=profile.release.codegen-units=1: generate the code in a single process to enable more opportunities for optimization
 # -Zbuild-std=std,panic_abort: rebuild the standard library with panic-abort behavior and our RUSTFLAGS
 
-if [[ $NO_STD == 1 ]]; then
+if [[ $NO_STD > 0 ]]; then
     RUSTFLAGS="-Zunstable-options -Cpanic=immediate-abort $RUSTFLAGS"
 fi
 
 cargo \
-    $( ([[ $TYPE == "static" ]] || [[ $NO_STD == 1 ]]) && echo "+$NIGHTLY" ) \
+    $( ([[ $TYPE == "static" ]] || [[ $NO_STD > 0 ]]) && echo "+$NIGHTLY" ) \
     "rustc" \
     "--manifest-path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/Cargo.toml" \
     "--crate-type=$( [[ "$TYPE" == "static" ]] && echo "staticlib" || echo "cdylib" )" \
@@ -34,8 +36,8 @@ cargo \
     "--config=profile.release.codegen-units=1" \
     "--no-default-features" \
     "--features=$( [[ $NO_STD == 1 ]] && echo "libc_alloc,looping_panic_handler" || echo "simple_logger"),$FEATURES" \
-    $( [[ $NO_STD = 1 ]] && echo "-Zbuild-std=core,alloc") \
-    $( ( [[ $TYPE = "static" ]] || [[ $NO_STD == 1 ]] ) && echo "-Zbuild-std=std,panic_abort" ) \
+    $( ( [[ $TYPE = "dynamic" ]] && [[ $NO_STD = 1 ]] ) && echo "-Zbuild-std=core,alloc") \
+    $( ( [[ $TYPE = "static" ]] || [[ $NO_STD > 0 ]] ) && echo "-Zbuild-std=std,panic_abort" ) \
     "--target=$TARGET" \
     "--" \
     "--emit" "link=$OUT"
